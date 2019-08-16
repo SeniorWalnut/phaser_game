e1166b43231c9d73a49ffc4da2045a254e82f1f9
Evgeny Agafonov <agafonov@etm-system.ru>
2019-08-16 16:24:12 +0200

    webpack change

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -12,8 +13,13 @@ module.exports = {
     rules: [
       {
         test: /\.js?$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+        include: resolve(__dirname, 'src/')
       }
     ]
   },
@@ -24,12 +30,15 @@ module.exports = {
   },
   devServer: {
   	contentBase: resolve(__dirname, 'dist'),
-  	port: 8080,
-  	compress: true
+  	port: 8080
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Phaser game'
-    })
+    }),
+    new CopyPlugin([{
+      from: resolve(__dirname, 'src', 'assets'),
+      to: 'assets'
+    }]),
   ]
 };
